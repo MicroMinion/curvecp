2bbfe14bc487f5e9103378ad14912600fc57cc27
Thomas Delaet <thomas@delaet.org>
2015-12-26 21:42:36 +0100

    Add example that allows to demonstrate client/server interaction while using JS implementation

---
+++ examples/js-test.js
@@ -0,0 +1,9 @@
+var UDPServer = require('./udp.js')
+
+var server = new UDPServer()
+
+var client = new UDPServer()
+
+setTimeout(function () {
+  client.connect({address: '127.0.0.1', port: server.getPort()}, server.keypair.publicKey)
+}, 1000)
