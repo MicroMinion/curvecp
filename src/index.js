010c72da953b07f84904b9c3bccfe70ed0a2ec11
Thomas Delaet <thomas@delaet.org>
2016-09-11 18:05:37 +0200

    export Message

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   PacketStream: require('./packet-stream.js'),
-  MessageStream: require('./message-stream.js')
+  MessageStream: require('./message-stream.js'),
+  Message: require('./message.js')
 }
